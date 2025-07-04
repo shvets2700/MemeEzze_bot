6835a7dee0aa1b3264d4407a88c123028346a4a5
shvets2700 <shvetsandrii50@gmail.com>
2025-07-03 22:11:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from dotenv import load_dotenv
+
+load_dotenv()  # Завантажити змінні з .env файлу
 
 # Отримуємо токен з середовища
-TOKEN = os.getenv("7995176267:AAFMhD79vq_DmYfMz7RZRpx7ID-algTdfBg")
+TOKEN = os.getenv("BOT_TOKEN")
 if not TOKEN:
-    raise ValueError("7995176267:AAFMhD79vq_DmYfMz7RZRpx7ID-algTdfBg")
+    raise ValueError("BOT_TOKEN is not set")
 
 # Створюємо застосунок (бота)
 app = ApplicationBuilder().token(TOKEN).build()
 
 # Обробник команди /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(" Бот працює!")
+    await update.message.reply_text("Бот працює!")
 
 # Додаємо обробник до застосунку
 app.add_handler(CommandHandler("start", start))

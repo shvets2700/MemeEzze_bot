2af76518b88baeb86d548a27c28b910ae0648cfb
shvets2700 <shvetsandrii50@gmail.com>
2025-07-03 22:41:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 # Беремо токен з оточення
 TOKEN = os.getenv("BOT_TOKEN")
 if not TOKEN:
-    raise ValueError("❌ BOT_TOKEN не передано! Перевір Environment Variables у Render.")
+    raise ValueError(" BOT_TOKEN не передано! Перевір Environment Variables у Render.")
 
 app = ApplicationBuilder().token(TOKEN).build()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ Бот працює!")
+    await update.message.reply_text(" Бот працює!")
 
 app.add_handler(CommandHandler("start", start))
 app.run_polling()
